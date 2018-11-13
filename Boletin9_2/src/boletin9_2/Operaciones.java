f182466966ace494e2fb2e2a8cae2b9e68685fd3
palonsovazquez <pav.vigo@gmail.com>
2018-11-13 12:08:59 +0100

    Boletin9_2 terminado

--- Boletin9_2/src/boletin9_2/Operaciones.java
+++ Boletin9_2/src/boletin9_2/Operaciones.java
@@ -5,6 +5,9 @@
  */
 package boletin9_2;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  *
  * @author palonsovazquez
@@ -15,16 +18,43 @@ public class Operaciones {
     }
     
     
-    public static void ejecutar(){
-   Double nInicio = 10d, nFinal = 90d, acumulaSuma = 0d, acumulaMult = 1d;
+    /*public static void ejecutar(){
+    Double nInicio = 10d, nFinal = 90d, acumulaSuma = 0d, acumulaMult = 1d;
     
     for(double i = nInicio; i <= nFinal;i++ ){
-        acumulaSuma += i;
-        acumulaMult *= i;
+    acumulaSuma += i;
+    acumulaMult *= i;
+    
+    
+    }
+    
+    System.out.printf("la suma es: %f y la multiplicacion es %.2f: ",acumulaSuma , acumulaMult);
+    
+    }*/
+    
+    
+    public static void ejecutar(){
+        Integer nInicio = 10, nFinal = 90;
+        BigInteger acumulaSuma = BigInteger.valueOf(0);
+        BigInteger acumulaMult = BigInteger.valueOf(1);
+    
+    for(Integer i = nInicio; i <= nFinal;i++ ){
+    acumulaSuma = acumulaSuma.add(BigInteger.valueOf(i));
+    acumulaMult = acumulaMult.multiply(BigInteger.valueOf(i));
+    
+    
         
+    }
+    BigDecimal acumulaMult2 = new  BigDecimal( acumulaMult);
+    Integer exponente = acumulaMult.toString().length() -1;
+    acumulaMult2 = acumulaMult2.divide(BigDecimal.valueOf(Math.pow(10, exponente)));
+    String multiplicacionFormateada = acumulaMult2.toPlainString().substring(0, 5) +" 10 \u00B9 " + exponente;
+    
+        
+        System.out.println("la suma es " +acumulaSuma+ " y la multiplicacion "+ acumulaMult2.toPlainString().substring(0, 5) +" *10"+ Peticiones.DevolverSuperIndice(exponente));
+    
     
     }
-        System.out.println("la suma es: "+ acumulaSuma + " y la multiplicacion es: "+ acumulaMult);
     
-    }
+    
 }
