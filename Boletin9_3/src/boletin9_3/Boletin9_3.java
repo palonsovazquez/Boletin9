9d65b360bf1e461a4ca75999a627948e2f4af6ce
Pablo Alonso Vázquez <43774387+palonsovazquez@users.noreply.github.com>
2018-11-19 12:24:55 +0100

    Update Boletin9_3.java

--- Boletin9_3/src/boletin9_3/Boletin9_3.java
+++ Boletin9_3/src/boletin9_3/Boletin9_3.java
@@ -15,7 +15,7 @@ public class Boletin9_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("El area del triangulo es "+ new Triangulo(Peticiones.pedirNumDoubleFiltrado("la base", Peticiones.CERO_POSITIVO),Peticiones.pedirNumDoubleFiltrado("la altura", Peticiones.CERO_POSITIVO)).getArea() );
+        System.out.println("El area del triangulo es "+ new Rectangulo(Peticiones.pedirNumDoubleFiltrado("la base", Peticiones.CERO_POSITIVO),Peticiones.pedirNumDoubleFiltrado("la altura", Peticiones.CERO_POSITIVO)).getArea() );
     }
     
 }
